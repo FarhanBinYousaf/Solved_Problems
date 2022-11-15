fb241f253ec739536177ffbf3e0263a3af389488
Farhan <mfarhanyousaf68@gmail.com>
2022-11-15 20:47:17 +0500

    commited

---
+++ kata3.py
@@ -0,0 +1,11 @@
+### Q : This kata is about multiplying a given number by eight if it is an even number 
+### and by nine otherwise.
+
+def multWithEight(num):
+    if num % 2 == 0:
+        result = num * 8
+    else:
+        result = num * 9
+    return result
+
+print(multWithEight(5))
